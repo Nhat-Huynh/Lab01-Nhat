2491c909707453c3af099474c9ccb267b599125f
Feyu205 <nhat.2373401010312@vanlanguni.vn>
2026-09-19 10:32:56 +0700

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@
  * @returns {boolean} true nếu đăng nhập hợp lệ, false nếu không
  */
 function checkLogin(username, password) {
-  if (username === 'admin' && password === '1234') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
