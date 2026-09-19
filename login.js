bf667098e001453e293e57be1991fed5a10f4062
Feyu205 <nhat.2373401010312@vanlanguni.vn>
2026-09-19 10:01:08 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+/**
+ * Kiểm tra thông tin đăng nhập.
+ * @param {string} username - Tên đăng nhập
+ * @param {string} password - Mật khẩu
+ * @returns {boolean} true nếu đăng nhập hợp lệ, false nếu không
+ */
+function checkLogin(username, password) {
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+  return false;
+}
+
+// Hỗ trợ dùng trong Node.js (Jest/Mocha) và trong trình duyệt
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { checkLogin };
+}
